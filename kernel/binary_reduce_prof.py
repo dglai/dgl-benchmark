30eb5ebbfaa1ead4cf7df6b2a09072d5df592138
Ubuntu <ubuntu@ip-172-31-3-16.us-east-2.compute.internal>
2019-09-09 03:32:17 +0000

    Update binary_reduce_prof.py

--- kernel/binary_reduce_prof.py
+++ kernel/binary_reduce_prof.py
@@ -1,7 +1,11 @@
 import dgl
 import dgl.function as fn
 import numpy as np
+import scipy as sp
 import torch as th
+
+import time
+from itertools import product
 
 D1 = 64
 D2 = 64
@@ -28,6 +32,9 @@ def max(x, dim):
 def min(x, dim):
     return x.min(dim)[0]
 
+def mean(x, dim):
+    return x.min(dim)[0]
+
 def prod(x, dim):
     return x.prod(dim)
 
@@ -45,6 +52,7 @@ udf_ops = {
     'sum': sum,
     'max': max,
     'min': min,
+    'mean': mean,
     'prod': prod,
     'matmul': matmul,
     'dot': dot,
@@ -59,83 +67,85 @@ def generate_feature(g, broadcast='none', binary_op='none', level=2):
     if level == 1:
         if binary_op == 'dot':
             if broadcast == 'e':
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D_EBED)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, 1, D_EBED)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D_EBED)))
+                u = th.randn((nv, D1, D_EBED))
+                e = th.randn((ne, 1, D_EBED))
+                v = th.randn((nv, D1, D_EBED))
             elif broadcast == 'u':
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, 1, D_EBED)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, D1, D_EBED)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D_EBED)))
+                u = th.randn((nv, 1, D_EBED))
+                e = th.randn((ne, D1, D_EBED))
+                v = th.randn((nv, D1, D_EBED))
             elif broadcast == 'v':
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D_EBED)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, D1, D_EBED)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, 1, D_EBED)))
+                u = th.randn((nv, D1, D_EBED))
+                e = th.randn((ne, D1, D_EBED))
+                v = th.randn((nv, 1, D_EBED))
             else:
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D_EBED)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, D1, D_EBED)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D_EBED)))
+                u = th.randn((nv, D1, D_EBED))
+                e = th.randn((ne, D1, D_EBED))
+                v = th.randn((nv, D1, D_EBED))
         else:
             if broadcast == 'e':
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, D1)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, 1)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, D1)))
+                u = th.randn((nv, D1))
+                e = th.randn((ne, 1))
+                v = th.randn((nv, D1))
             elif broadcast == 'u':
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, 1)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, D1)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, D1)))
+                u = th.randn((nv, 1))
+                e = th.randn((ne, D1))
+                v = th.randn((nv, D1))
             elif broadcast == 'v':
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, D1)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, D1)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, 1)))
+                u = th.randn((nv, D1))
+                e = th.randn((ne, D1))
+                v = th.randn((nv, 1))
             else:
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, D1)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, D1)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, D1)))
+                u = th.randn((nv, D1))
+                e = th.randn((ne, D1))
+                v = th.randn((nv, D1))
     else:
         if binary_op == 'dot':
             if broadcast == 'e':
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D2, D_EBED)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, D1, 1, D_EBED)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D2, D_EBED)))
+                u = th.randn((nv, D1, D2, D_EBED))
+                e = th.randn((ne, D1, 1, D_EBED))
+                v = th.randn((nv, D1, D2, D_EBED))
             elif broadcast == 'u':
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, 1, D_EBED)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, D1, D2, D_EBED)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D2, D_EBED)))
+                u = th.randn((nv, D1, 1, D_EBED))
+                e = th.randn((ne, D1, D2, D_EBED))
+                v = th.randn((nv, D1, D2, D_EBED))
             elif broadcast == 'v':
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D2, D_EBED)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, D1, D2, D_EBED)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, D2, 1, D_EBED)))
+                u = th.randn((nv, D1, D2, D_EBED))
+                e = th.randn((ne, D1, D2, D_EBED))
+                v = th.randn((nv, D2, 1, D_EBED))
             else:
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D2, D_EBED)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, D1, D2, D_EBED)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D2, D_EBED)))
+                u = th.randn((nv, D1, D2, D_EBED))
+                e = th.randn((ne, D1, D2, D_EBED))
+                v = th.randn((nv, D1, D2, D_EBED))
         else:
             if broadcast == 'e':
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D2)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, 1, D2)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D2)))
+                u = th.randn((nv, D1, D2))
+                e = th.randn((ne, 1, D2))
+                v = th.randn((nv, D1, D2))
             elif broadcast == 'u':
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, 1, D2)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, D1, D2)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D2)))
+                u = th.randn((nv, 1, D2))
+                e = th.randn((ne, D1, D2))
+                v = th.randn((nv, D1, D2))
             elif broadcast == 'v':
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D2)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, D1, D2)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, 1, D2)))
+                u = th.randn((nv, D1, D2))
+                e = th.randn((ne, D1, D2))
+                v = th.randn((nv, 1, D2))
             else:
-                u = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D2)))
-                e = th.from_numpy(np.random.uniform(-1, 1, (ne, D1, D2)))
-                v = th.from_numpy(np.random.uniform(-1, 1, (nv, D1, D2)))
+                u = th.randn((nv, D1, D2))
+                e = th.randn((ne, D1, D2))
+                v = th.randn((nv, D1, D2))
     return u, v, e
 
 
 def prof_binary_builtins():
-    def _prof(g, lhs, rhs, binary_op, reducer, broadcast='none', level=1):
+    def _prof(g, lhs, rhs, binary_op, reducer, broadcast='none', level=1, dev='cuda:0'):
         hu, hv, he = generate_feature(g, broadcast, binary_op, level)
 
-        g.ndata['u'] = hu
-        g.ndata['v'] = hv
-        g.edata['e'] = he
+        g.ndata['u'] = hu.requires_grad_()
+        g.ndata['v'] = hv.requires_grad_()
+        g.edata['e'] = he.requires_grad_()
+
+        g.to(th.device(dev))
 
         builtin_msg_name = "{}_{}_{}".format(lhs, binary_op, rhs)
         builtin_msg = getattr(fn, builtin_msg_name)
@@ -149,30 +159,27 @@ def prof_binary_builtins():
             else:
                 return g.edata["e"]
 
-        def time_forward():
-            dur = []
+        def time_forward_backward():
+            dur_f = []
+            dur_b = []
             for i in range(50):
                 if i >= 5:
                     t0 = time.time()
                 g.update_all(builtin_msg(lhs, rhs, 'm'), builtin_red('m', 'r1'))
                 r1 = g.ndata.pop('r1')
                 if i >= 5:
-                    dur.append(time.time() - t0)
-            return np.average(dur), r1.sum()
+                    dur_f.append(time.time() - t0)
 
-        def time_backward(grad):
-            dur = []
-            for i in range(50):
+                grad = r1.sum()
+
                 if i >= 5:
                     t0 = time.time()
                 grad.backward()
                 if i >= 5:
-                    dur.append(time.time() - t0)
-            return np.average(dur)
+                    dur_b.append(time.time() - t0)
+            return np.average(dur_f), np.average(dur_b)
 
-        builtin_forward_time, grad = time_forward()
-        builtin_backward_time = time_backward(grad)
-
+        builtin_forward_time, builtin_backward_time = time_forward_backward()
         def target_switch(edges, target):
             if target == "u":
                 return edges.src
@@ -188,9 +195,9 @@ def prof_binary_builtins():
             lhs_data = target_switch(edges, lhs)[lhs]
             rhs_data = target_switch(edges, rhs)[rhs]
 
-            while F.ndim(lhs_data) < F.ndim(rhs_data):
+            while lhs_data.dim() < rhs_data.dim():
                 lhs_data = F.unsqueeze(lhs_data, 1)
-            while F.ndim(rhs_data) < F.ndim(lhs_data):
+            while rhs_data.dim() < lhs_data.dim():
                 rhs_data = F.unsqueeze(rhs_data, 1)
             return {"m": op(lhs_data, rhs_data)}
 
@@ -198,46 +205,46 @@ def prof_binary_builtins():
             op = udf_ops[reducer]
             return {"r2": op(nodes.mailbox['m'], 1)}
 
-        def udf_time_forward():
-            dur = []
+        def udf_time_forward_backward():
+            dur_f = []
+            dur_b = []
             for i in range(50):
                 if i >= 5:
                     t0 = time.time()
                 g.update_all(mfunc, rfunc)
                 r2 = g.ndata.pop('r2')
                 if i >= 5:
-                    dur.append(time.time() - t0)
-            return np.average(dur), r2.sum()
+                    dur_f.append(time.time() - t0)
 
-        def udf_time_backward(grad):
-            dur = []
-            for i in range(50):
+                grad = r2.sum()
+
                 if i >= 5:
                     t0 = time.time()
                 grad.backward()
                 if i >= 5:
-                    dur.append(time.time() - t0)
-            return np.average(dur)
+                    dur_b.append(time.time() - t0)
+            return np.average(dur_f), np.average(dur_b)
 
-        udf_forward_time, grad = udf_time_forward()
-        udf_backward_time = udf_time_backward(grad)
-        print("--------------------------------------------------------------------------")
-        print("{}_{}_{}_{}_{} | {%.3f} | {%.3f} | {%.3f} | {%.3f} ", 
-            format(lhs, binary_op, rhs, reducer, broadcast,
-                udf_forward_time, builtin_forward_time,
-                udf_backward_time, builtin_backward_time))
+        udf_forward_time, udf_backward_time = udf_time_forward_backward()
+        print("--------------------------------------------------------------")
+        print("{}_{} | {} | {}_{}_{} | {} | {} | {:06.5f} | {:06.5f} | {:06.5f} | {:06.5f} ".
+            format(D1, D2, binary_op, lhs, binary_op, rhs, reducer, broadcast,
+                udf_forward_time.astype(float), builtin_forward_time.astype(float),
+                udf_backward_time.astype(float), builtin_backward_time.astype(float)))
 
     adj = sp.sparse.random(1000, 1000, 0.05)
     G = dgl.DGLGraph(adj)
     target = ["u", "v", "e"]
-    for level in [1,2]:
-        for lhs, rhs in product(target, target):
-            if lhs == rhs:
-                continue
-            for binary_op in ["add", "sub", "mul", "div", "dot"]:
-                for reducer in ["sum", "max", "min", "mean"]:
-                    for broadcast in ["none", lhs, rhs]:
-                         _test(G, lhs, rhs, binary_op, reducer, broadcast=broadcast)
+    for size in [64, 128, 256]:
+        D1 = size
+        for level in [1,2]:
+            for lhs, rhs in product(target, target):
+                if lhs == rhs:
+                    continue
+                for binary_op in ["add", "sub", "mul", "div", "dot"]:
+                    for reducer in ["sum", "max", "min", "mean"]:
+                        for broadcast in ["none", lhs, rhs]:
+                            _prof(G, lhs, rhs, binary_op, reducer, broadcast=broadcast)
 
 if __name__ == '__main__':
     prof_binary_builtins()
