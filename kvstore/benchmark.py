e9026e2c19c421f9f1381e73c22ceb12fb2a98cb
aksnzhy <mctt90@gmail.com>
2020-07-03 15:46:15 +0800

    update

--- kvstore/benchmark.py
+++ kvstore/benchmark.py
@@ -244,7 +244,7 @@ class ArgParser(argparse.ArgumentParser):
                           help='data_size of each machine.')
         self.add_argument('--dim', type=int, default=10,
                           help='dim of each data.')
-        self.add_argument('--graph_size', type=int, default=1000000,
+        self.add_argument('--graph_size', type=int, default=100000000,
                           help='total size of the graph.')
         self.add_argument('--threads', type=int, default=-1,
                           help='number of pytorch threads.')
